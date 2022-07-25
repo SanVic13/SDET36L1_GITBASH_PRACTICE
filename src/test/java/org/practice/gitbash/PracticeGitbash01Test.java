02477aabca76c24f29c9abd8134408caa062ada9
SanVic13 <pandeysandeep910@gmail.com>
2022-07-25 15:58:17 +0530

    first commit

---
+++ src/test/java/org/practice/gitbash/PracticeGitbash01Test.java
@@ -0,0 +1,12 @@
+package org.practice.gitbash;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PracticeGitbash01Test {
+
+	@Test
+	public void practiceGithub01() {
+		Reporter.log("practice github 01", true);
+	}
+}
