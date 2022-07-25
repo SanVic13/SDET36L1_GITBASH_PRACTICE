d8552eac747fc0d965e015ce8712c26ce152b84a
SanVic13 <pandeysandeep910@gmail.com>
2022-07-25 16:08:18 +0530

    second updated

--- src/test/java/org/practice/gitbash/PracticeGitbash02Test.java
+++ src/test/java/org/practice/gitbash/PracticeGitbash02Test.java
@@ -1,5 +1,12 @@
 package org.practice.gitbash;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class PracticeGitbash02Test {
 
+	@Test
+	public void practiceGitbash01() {
+		Reporter.log("practice gitbash 01-01", true);
+	}
 }
